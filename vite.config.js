118c514b11803898ebb156def0500ec71f9c2f5a
vzahriichuk <vzahriichuk@binariks.com>
2025-05-26 16:03:45 +0300

    maybe right now

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
+// vite.config.js
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        task1: "task1.html",
+        task2: "task2.html",
+        task3: "task3.html",
+        task4: "task4.html",
+      },
+    },
+  },
   plugins: [
     basicSsl({
       /** name of certification */
       name: "test",
       /** custom trust domains */
       domains: ["*.custom.com"],
-      /** custom certification directory */
-      certDir: "/Users/.../.devServer/cert",
     }),
   ],
 };
